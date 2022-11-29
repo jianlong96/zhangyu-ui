d503f30e299eb8127634121e53b9f2b9b70ea620
jianlong96 <liujianlong_hw_hrb@163.com>
2022-11-29 16:18:07 +0800

    修复md显示bug

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,10 @@
-declare module '*.vue'{
-    import { ComponentOptions } from "vue"
-    const componentOptions:ComponentOptions
-    export default componentOptions
+declare module "*.vue" {
+  import { ComponentOptions } from "vue";
+  const componentOptions: ComponentOptions;
+  export default componentOptions;
 }
-declare module 'prismjs'
+declare module "prismjs";
+declare module "*.md" {
+  const str: string;
+  export default str;
+}
